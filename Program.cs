810ab2095e2ecc21ae08213f4eafdc94cb1798b7
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2020-06-03 21:18:45 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MilhasParaKm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digíte um valor de milha,como exemplo 1, 2, 3...");
+            Console.Write("Escreva o valor de uma milha:");
+            double M=Convert.ToDouble(Console.ReadLine());
+            double K=1.609;
+            double resultado=K * M;
+            Console.Clear();
+            Console.Write($"Resultado da sua milha para quilômetros= {resultado}km");
+            
+        }
+    }
+}
